0b25d3a9e3494868ffa457e46a5ba3ce4211ebaa
Yordan <danione@abv.bg>
2017-02-07 21:50:23 +0200

    Changed models, updated views, to be continued

--- thesis_manager/admin_panel/models.py
+++ thesis_manager/admin_panel/models.py
@@ -1,36 +1,48 @@
 from django.db import models
+import datetime
 
-class ManagmentAndReview(models.Model):
-    personal_id = models.AutoField(primary_key=True)
-    personal_name = models.CharField(max_length=200)
-    titles = models.CharField(max_length=200)
-    place_of_work = models.CharField(max_length=200)
-    category = models.CharField(max_length=60)
+
+class BasicModel(models.Model):
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 300, default = "none")
+    category = models.CharField(max_length = 60, default = "System Programming")
+
+    class Meta:
+        abstract = True
+
+class ManagmentAndReview(BasicModel):
+    titles = models.CharField(max_length=100)
+    workplace = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.personal_name
+        return self.name
 
-class Thesis(models.Model):
-    thesis_id = models.AutoField(primary_key=True)
-    thesis_description = models.TextField()
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, null=True)
-    category = models.CharField(max_length=60)
+class Thesis(BasicModel):
+    supervisor = models.ForeignKey(ManagmentAndReview, on_delete=models.CASCADE, null=True)
+
     def __str__(self):
-        return self.thesis_description
+        return self.name
 
-class Student(models.Model):
-    student_id = models.AutoField(primary_key=True)
-    student_name = models.CharField(max_length=200)
-    student_class = models.CharField(max_length=1)
-    student_number = models.IntegerField()
-    class_type = models.CharField(max_length=200)
-    handed_document_over = models.BooleanField(default=False)
-    handed_assignment_over = models.BooleanField(default=False)
-    has_commission = models.BooleanField(default=False)
-    has_reviewer = models.BooleanField(default=False)
-    assigned_teacher = models.ForeignKey(ManagmentAndReview, on_delete=models.CASCADE, null=True)
+class Student(BasicModel):
+    class_char = models.CharField(max_length = 1)
+    number = models.IntegerField()
+    handed_document_over = models.BooleanField(default = False)
+    handed_assignment_over = models.BooleanField(default = False)
+    assigned_supervisor = models.ForeignKey(ManagmentAndReview, on_delete=models.CASCADE, null=True, related_name = "supervisor")
     assigned_thesis = models.ForeignKey(Thesis, on_delete=models.CASCADE, null=True)
-    assigned_reviewer = models.ForeignKey(ManagmentAndReview, on_delete=models.CASCADE, null=True)
+    assigned_reviewer = models.ForeignKey(ManagmentAndReview, on_delete=models.CASCADE, null=True, related_name = "reviewer")
+    study_period = models.CharField(max_length = 30,default = str(datetime.datetime.now().year) + "-" + str(datetime.datetime.now().year + 1))
+    did_graduate = models.BooleanField(default = False)
 
     def __str__(self):
-        return self.student_name
+        return self.name
+
+class Commission(BasicModel):
+    chairman = models.ForeignKey(ManagmentAndReview, on_delete=models.CASCADE, null = True)
+    commissioners = models.ManyToManyField(ManagmentAndReview, related_name = "commissioners")
+    place = models.CharField(max_length = 30, default = "none")
+    time = models.DateTimeField(null = True)
+    students = models.ManyToManyField(Student, related_name = "students")
+
+    def __str__(self):
+        return self.name
--- thesis_manager/admin_panel/views.py
+++ thesis_manager/admin_panel/views.py
@@ -3,7 +3,7 @@
 # Archives
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
-from .models import Student, ManagmentAndReview, Thesis
+from .models import Student, ManagmentAndReview, Thesis, Commission
 import csv
 import os
 
@@ -12,72 +12,66 @@ def get_archive_dir():
     return os.getcwd() + '/archives/'
 
 def init_list():
-    return list(Student.objects.order_by('student_class','student_name'))
-
-def delete_information():
-    Student.objects.all().delete()
+    return list(Student.objects.order_by('class_char','name'))
 
 
 def admin_homepage(request):
     students_list_document = init_list()
     students_list_assignment = init_list()
+    students_list_documentation = init_list()
     students_list_reviewer = init_list()
     students_list_commission  = init_list()
 
     students_list_document = [student for student in students_list_document if student.handed_document_over == False]
     students_list_assignment = [student for student in students_list_assignment if student.handed_assignment_over == False]
-    students_list_reviewer = [student for student in students_list_reviewer if student.has_reviewer == False]
-    students_list_commission = [student for student in students_list_commission if student.has_commission == False]
+    students_list_documentation = [student for student in students_list_documentation if student.assigned_supervisor is None]
+    students_list_reviewer = [student for student in students_list_reviewer if student.assigned_reviewer is None]
+    students_list_commission = [student for student in students_list_commission if student.assigned_commission is None]
 
     context = {'students_list_document': students_list_document,'students_list_assignment' : students_list_assignment,
-               'students_list_reviewer': students_list_reviewer,'students_list_commission' : students_list_commission}
+               'students_list_documentation': students_list_documentation,'students_list_reviewer': students_list_reviewer,
+               'students_list_commission' : students_list_commission}
     return render(request, 'admin_homepage.html', context)
-# We need to keep it secret, keep it safe! Every student gets a field -> graduated? and it is PUT!
+
 def empty_tables(request):
-    delete_information()
     return HttpResponseRedirect('redirection')
 
 def new_year(request):
     return render(request, 'new_year.html')
 
-def deleting_database(request):
-    if request.method == 'POST':
-        directory = get_archive_dir()
-        os.remove(directory + request.POST.get('delete'))
-        return HttpResponseRedirect('redirection')
-
-    return (request,'man_years.html')
+# def load_database(request):
+#     if request.method == 'POST':
+#         directory = get_archive_dir()
+#         delete_information()
+#         with open(directory + request.POST.get('load'), 'r', newline='') as csvfile:
+#             reader = csv.reader(csvfile)
+#             for row in reader:
+#                 Student(student_name = row[1], student_class = row[2], student_number = row[0], category = row[3], handed_document_over = row[4], handed_assignment_over = row[5],
+#                 has_commission = row[6], has_reviewer = row[7]).save()
+#             return HttpResponseRedirect('redirection')
+#     return (request, 'man_years.html')
 
 def load_database(request):
-    if request.method == 'POST':
-        directory = get_archive_dir()
-        delete_information()
-        with open(directory + request.POST.get('load'), 'r', newline='') as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                Student(student_name = row[1], student_class = row[2], student_number = row[0], category = row[3], handed_document_over = row[4], handed_assignment_over = row[5],
-                has_commission = row[6], has_reviewer = row[7]).save()
-            return HttpResponseRedirect('redirection')
     return (request, 'man_years.html')
-
 
 def saving_database(request):
     if request.method == 'POST':
-        raw_name = request.POST.get('SaveYear')
-        students_list = init_list()
-        directory = get_archive_dir()
+        # raw_name = request.POST.get('SaveYear')
+        # students_list = init_list()
+        # directory = get_archive_dir()
+        #
+        # if not os.path.exists(directory):
+        #     os.makedirs(directory)
+        #
+        # with open(directory + raw_name + '.csv', 'w+', newline='') as csvfile:
+        #     writer = csv.writer(csvfile)
+        #
+        #     for student in students_list:
+        #         writer.writerow([student.student_number,student.student_name, student.student_class, student.category, student.handed_document_over,student.handed_assignment_over,
+        #         student.has_reviewer, student.has_commission])
+        #     return HttpResponseRedirect('redirection')
 
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
-        with open(directory + raw_name + '.csv', 'w+', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-
-            for student in students_list:
-                writer.writerow([student.student_number,student.student_name, student.student_class, student.category, student.handed_document_over,student.handed_assignment_over,
-                student.has_reviewer, student.has_commission])
-            return HttpResponseRedirect('redirection')
-
+        return HttpResponseRedirect('redirection')
     return (request,'man_years.html')
 
 def man_years(request):
@@ -92,11 +86,9 @@ def student_handler(request):
         name_ = request.POST.get('FullName')
         class_ = request.POST.get('Class')
         number_ = request.POST.get('Number')
-        software_ = False;
-        if request.POST.get('software-hardware') == 0:
-            software_ = True;
-            Student(student_name = name_ , student_class = class_ , student_number = number_ , system_programming = software_).save()
-            return HttpResponseRedirect('redirection')
+        category_ = request.POST.get('Category')
+        Student(name = name_ , class_char = class_ , number = number_ , category = category_).save()
+        return HttpResponseRedirect('redirection')
 
     return(request, 'upload_student.html')
 
@@ -107,20 +99,20 @@ def file_handler(request):
             my_file = request.FILES['text_csv'].read().decode('cp1251')
             rows = my_file.split('\r\n')
             class_ = None
-            software_ = False
+            category_ = None
             for row in rows:
                 if row[:1] == 'X':
                     class_ = row[-1:].upper()
                 elif row[:1] == 'S':
-                    software_ = True
+                    category_ = 'System Programming'
                 elif row[:1] == 'H':
-                    software_ = False
+                    category_ = 'Computer Networks'
                 else:
                     if len(row) > 1:
                         information = row.split('.')
                         number_ = information[0]
                         student_ = information[1].replace(' ','',1)
-                        Student(student_name=student_, student_class=class_, student_number = number_, system_programming = software_ ).save()
+                        Student(name=student_, category = category_ , class_char=class_, number = number_,).save()
         return HttpResponseRedirect('redirection')
     return(request,'upload_student.html')
 
@@ -130,18 +122,15 @@ def upload_students(request):
 def teacher_handler(request):
     if request.method == 'POST':
         name_ = request.POST.get('FullName')
+        category_ = request.POST.get('Category');
         titles_ = request.POST.get('Title')
-        work_ = request.POST.get('Work')
-        software_ = False;
-        if request.POST.get('software-hardware') == 0:
-            software_ = True;
-        Teacher(teacher_name = name_ , titles = titles_ , place_of_work = work_ , system_programming = software_).save()
+        workpace_ = request.POST.get('Work')
+        ManagmentAndReview(name = name_ , category = category_, titles = titles_ , workplace = work_ , ).save()
         return HttpResponseRedirect('redirection')
     return(request, 'upload_teacher.html')
 
 def upload_teachers(request):
     return render(request, 'upload_teacher.html')
-
 
 def upload_thesis(request):
     return render(request, 'upload_thesis.html')
@@ -152,9 +141,10 @@ def upload_reviewers(request):
 def reviewer_handler(request):
     if request.method == 'POST':
         name_ = request.POST.get('FullName')
+        category_ = request.POST.get('Category')
         titles_ = request.POST.get('Title')
-        work_ = request.POST.get('Work')
-        Reviewer(teacher_name = name_ , titles = titles_ , place_of_work = work_).save()
+        workplace_ = request.POST.get('Work')
+        ManagmentAndReview(name = name_ ,category = category_, titles = titles_ , workplace = workpace_).save()
         return HttpResponseRedirect('redirection')
     return(request, 'upload_reviewer.html')
 
@@ -163,13 +153,8 @@ def reviewer_handler(request):
 def thesis_handler(request):
     if request.method == 'POST':
         description_ = request.POST.get('ThesisDescription')
-        software_ = False;
-        hardware_ = False;
-        if request.POST.get('software-hardware') == 0:
-            software_ = True;
-        elif request.POST.get('software-hardware') == 1:
-            hardware_ = True;
-        Thesis(thesis_description = description_, software = software_, hardware = hardware_).save()
+        category_ = request.POST.get('Category')
+        Thesis(name = description_, category = category_).save()
         return HttpResponseRedirect('redirection')
 
     return(request, 'upload_thesis.html')
